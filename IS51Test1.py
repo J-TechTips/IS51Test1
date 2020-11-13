633d04d06c1c72d996061f2c29cdfbaf0712e114
J-TechTips <jtecktips@gmail.com>
2020-11-12 17:27:38 -0800

    psuedo-code completed

--- IS51Test1.py
+++ IS51Test1.py
@@ -10,3 +10,29 @@ Print 'Option 1 and Option 2 pays the same' if they are equal
 With two options there are two functions 
 One for option1 and One for option 2 
 """
+
+"""
+Option 1 
+return 100*10
+
+Option 2 
+amount = 1
+list1 = []
+loop 10 times 
+    add amount to list1 
+    amount *=  2
+    sum = all items inside of loop
+return sum 
+main 
+    var1 = option1
+    var2 = option2 
+
+if var1 > var2 
+    print "Option 1 is better"
+if var1 < var2 
+    print "Option 2 is better"
+else 
+    "Either option is fine"
+
+main 
+"""
