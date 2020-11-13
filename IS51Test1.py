a1c88a73df0bb82458adee1ebfd51caef1c70c6a
J-TechTips <jtecktips@gmail.com>
2020-11-12 17:10:33 -0800

    structured-english

---
+++ IS51Test1.py
@@ -0,0 +1,12 @@
+
+"""
+Show two options for salary 
+Option 1 is $100 per day 
+Option 2 is $1 the first day, $2 the second day, $4 the third day, and so on (doubles each day)
+Determine with option pays more by calculating 
+Print either 'Option 1 is better' or 'Option 2 is better'
+Print 'Option 1 and Option 2 pays the same' if they are equal
+
+With two options there are two functions 
+One for option1 and One for option 2 
+"""
